7e1b2289b54689036516d976f20b657dfda4305a
CappieGold <144028326+CappieGold@users.noreply.github.com>
2026-02-24 16:01:25 +0000

    feat: add css for component & initialize App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
-function App() {
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userUsername, setUserUsername] = useState("")
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('accessToken'))
+    axios.post('/api/auth/', {}, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then((response) => {
+        setIsLoggedIn(true)
+        setUserUsername(response.data.username)
+      })
+  }, [])
+
   return (
     <div className="App">
+      {isLoggedIn ? <Dashboard /> : <Authentication />}
     </div>
   )
 }
-
-export default App
