de524f06d9b4ecb072835c86d154e0e4fabc29e0
CappieGold <144028326+CappieGold@users.noreply.github.com>
2026-02-26 11:44:07 +0000

    feat: refactor routing structure in Dashboard component and remove BrowserRouter from main

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import './dashboard.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Header from '../../components/navigation/Header'
 import SideBar from '../../components/navigation/SideBar'
 
 export default function Dashboard({ userUsername, setIsLoggedIn }) {
     return (
-        <div className='dashboard'>
-            <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn}/>
-            <SideBar />
-        </div>
+        <BrowserRouter>
+            <div className='dashboard'>
+                <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+                <SideBar />
+                <Routes>
+                    <Route path="/home" />
+                    <Route path="/favorites" />
+                    <Route path="/watchlater" />
+                    <Route path="*" element={<Navigate to="/home" replace />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     )
 }
